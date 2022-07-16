32429eb07c94c1e3834961900a0be08e664348f0
GoldAbsolute <sokoldima1998@mail.ru>
2022-07-16 23:39:34 +0300

    fix

--- custom-sorting/main.go
+++ custom-sorting/main.go
@@ -1,1 +1,24 @@
 package custom_sorting
+
+import (
+	"fmt"
+	"sort"
+)
+
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func Main() {
+	fruits := []string{"peach", "banana", "kiwi"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
